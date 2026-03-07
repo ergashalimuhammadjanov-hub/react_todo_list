980cef5ac2a677894f041e4ee5df6d1145aac0e6
Ergash Aliy <ergashalimuhammadjanov@gmail.com>
2026-03-07 20:34:01 +0500

    yangilangan todo

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,21 @@ function App() {
   const [mode, setMode] = useState(localStorage.getItem("theme"))
   localStorage.setItem("theme", mode)
 
+  const [title, setTitle] = useState("")
+
+  const [data, setData] = useState([
+    {
+      id: 1,
+      title: "kitob o'qish",
+      isDone: true
+    },
+    {
+      id: 2,
+      title: "mashq",
+      isDone: false
+    }
+  ])
+
   return (
     <div className={mode == "dark" ? 'body active' : "body"}>
       <div className="container">
@@ -33,28 +48,61 @@ function App() {
         </div>
         <main>
           <div className="rejalar">
-            <div className="todo">
-              <div className="list">
-                <input onChange={(e) => setBelgilandi(e.target.checked)} className='checkbox' type="checkbox" />
-                <h3 style={{ opacity: belgilandi ? 0.5 : 1, textDecoration: belgilandi ? "line-through" : "none" }}>Note #1</h3>
-              </div>
-              <div className="tugma">
-                <img src="/imgs/Frame 6.svg" alt="" title='editing' />
-                <img src="/imgs/trash.svg" alt="" title='trash' />
-              </div>
-            </div>
+            {
+              data.map((item) => {
+                return <div key={item.id} className="todo">
+                  <div className={`list ${item.isDone ? "active" : ""}`}>
+                    <input
+                      type="checkbox"
+                      checked={item.isDone}
+                      onChange={() =>
+                        setData(data.map(el =>
+                          el.id === item.id ? { ...el, isDone: !el.isDone } : el
+                        ))
+                      }
+                    />
+                    <h3>{item.title}</h3>
+                  </div>
+                  <div className="tugma">
+                    <img src="/imgs/Frame 6.svg" alt="" title='editing' />
+                    <img src="/imgs/trash.svg" alt="" title='trash' />
+                  </div>
+                </div>
+              })
+
+            }
             <button onClick={() => setBosish(true)}
               className="addbtn"><img src="/imgs/plusbtnimg.svg" alt="" /></button>
           </div>
           {bosildi ? <div className={`overlay`}>
             <div className="modal">
               <h2>New Note</h2>
-              <form action="">
-                <input type="text" placeholder="Input your note..." />
+
+              <form onSubmit={(e) => {
+                e.preventDefault()
+                if (!title.trim()) {
+                  alert("Bo'sh qiymat kiritib bo'lmaydi !!!")
+                  return
+                } else {
+                  const obj = {
+                    id: data.length + 1,
+                    title: title,
+                    isDone: false
+                  }
+
+                  setData([...data, obj])
+                  setTitle("")
+                  setBosish(false)
+                }
+
+              }} action="">
+                <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Input your note..." />
                 <div className="modalbtns">
-                  <button className="canel">Cancel</button>
+                  <button type="button" className="canel" onClick={() => setBosish(false)}>Cancel</button>
                   <button className="apply">Apply</button>
                 </div>
+
+
               </form>
             </div>
           </div> : ""}
